dcc56edd5cbf8e7765d2404ebf24d9354aa0abe3
Janell Anderson <andersonjanell@gmail.com>
2018-08-31 17:56:11 -0400

    added style, h1 and p tags. Also added pag-2 file fas a link.

--- tutorial-part-one/src/pages/index.js
+++ tutorial-part-one/src/pages/index.js
@@ -1,3 +1,23 @@
 import React from "react"
+import Link from "gatsby-link";
 
-export default () => <div>Hello world!</div>
+export default () => 
+<div style = {{ color: `tomato` }}>
+    <h1>'Hello world!</h1>
+    <p> What a world</p>  
+    <img src = "https://source.unsplash.com/random/400x200" alt = "" />
+    <br />
+    <div>
+        <Link to ="/page-2">Link</Link>
+    </div>
+</div>
+
+// Tutorial Steps taken
+/* 
+
+1) Added style to Div
+2) Added h1 tags and p tags
+3) Added image
+4) Imported Link to add links
+
+*/
