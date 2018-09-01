de750a4b083745ad9d38e2c1335ae8532a3a9ac0
Janell Anderson <andersonjanell@gmail.com>
2018-08-31 20:03:09 -0400

    Component counter has a state or count where I can plus or minus the counter from 0.

---
+++ tutorial-part-one/src/pages/counter.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Link from "gatsby-link";
+
+class Counter extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>Counter</h1>
+                <p>current count: {this.state.count}</p>
+                <button 
+                    onClick={() =>
+                        this.setState(prevState => ({
+                            count: prevState.count + 1,
+                        }))
+                    }>
+                    plus </button>
+                <button 
+                    onClick={() =>
+                        this.setState(prevState => ({
+                            count:prevState.count - 1,
+                        }))
+                     }>
+                    minus</button>
+            </div>
+        )
+    }
+}
+
+export default Counter;
+
+// Made a component that is a counter
+// Made two buttons that will add and minus from the counter
+// Then made a constructor with a state of counter: 0
+// Then change the count in the p tag to this.state.count
